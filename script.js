d2c104f7a7f2e595509ec8ade3487baca51a417d
mitumial <ace.vortex76@gmail.com>
2024-08-03 16:52:01 -0500

    add functional encryption

--- script.js
+++ script.js
@@ -3,8 +3,31 @@ let outputMessage = document.querySelector("#output-message");
 
 document.querySelector("#btn-encrypt").addEventListener("click", function () {
     document.querySelector(".output-group__img").style.display = "none";
-    outputMessage.innerHTML = inputMessage.value;
+    let textToEncrypt = inputMessage.value;
+    let encryptedText = "";
+    textToEncrypt.split("").forEach((letter) => {
+        encryptedText += cipherLetter(letter);
+    });
+
+    outputMessage.innerHTML = encryptedText;
 });
+
+function cipherLetter(letter) {
+    switch (letter) {
+        case "a":
+            return "ai";
+        case "e":
+            return "enter";
+        case "i":
+            return "imes";
+        case "o":
+            return "ober";
+        case "u":
+            return "ufat";
+        default:
+            return letter;
+    }
+}
 
 document.querySelector("#btn-decrypt").addEventListener("click", function () {
     document.querySelector(".output-group__img").style.display = "none";
